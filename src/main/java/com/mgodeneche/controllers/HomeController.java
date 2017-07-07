f302e7a7dca3fa5f47e609a8055eb18ddda1b176
Godeneche Maxence <maxence.godeneche@gmail.com>
2017-07-07 02:19:22 +0200

    Original commit
    First commit

---
+++ src/main/java/com/mgodeneche/controllers/HomeController.java
@@ -0,0 +1,74 @@
+package com.mgodeneche.controllers;
+
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mgodeneche.entity.Research;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value="/newSearch")
+	public String createNewResearch(HttpServletRequest  req, ModelMap model){
+		instanciateResearch(req);
+		//TODO persist
+		
+		return null;
+	}
+	@RequestMapping(value="/home")
+	public String getResearchList(HttpServletRequest  req, ModelMap model){
+		//TODO connexionBDD
+		List<Research> researchlist = new ArrayList();
+		model.addAttribute("researchList",researchlist);
+		
+		return "list";
+	}
+	private Research instanciateResearch(HttpServletRequest  req){
+		Research research = new Research();
+		research.setRegion((String) req.getAttribute("region"));
+		research.setDepartement((String) req.getAttribute("dept"));
+		research.setKeywords((String) req.getAttribute("keywords"));
+		research.setMinPrice((Float) req.getAttribute("minPrice"));
+		research.setMaxPrice((Float) req.getAttribute("maxPrice"));
+		research.setStartDate(LocalDateTime.now());
+		research.setValid(true);
+		
+		return research;
+	}
+}
---
+++ src/main/java/com/mgodeneche/entity/Research.java
@@ -0,0 +1,85 @@
+package com.mgodeneche.entity;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Research {
+
+	private String region;
+	private String departement;
+	private String keywords;
+	private Float minPrice;
+	private Float maxPrice;
+	private Integer maxResult;
+	private LocalDateTime startDate;
+	private Boolean valid;
+	
+	
+	public Research(){
+		super();
+		
+	}
+	
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getDepartement() {
+		return departement;
+	}
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String item) {
+		this.keywords = item;
+	}
+	public Float getMinPrice() {
+		return minPrice;
+	}
+	public void setMinPrice(Float minPrice) {
+		this.minPrice = minPrice;
+	}
+	public Float getMaxPrice() {
+		return maxPrice;
+	}
+	public void setMaxPrice(Float maxPrice) {
+		this.maxPrice = maxPrice;
+	}
+	public Integer getMaxResult() {
+		return maxResult;
+	}
+	public void setMaxResult(Integer maxResult) {
+		this.maxResult = maxResult;
+	}
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+
+	public Boolean getValid() {
+		return valid;
+	}
+
+	public void setValid(Boolean valid) {
+		this.valid = valid;
+	}
+
+	@Override
+	public String toString() {
+		return "Research on ["+keywords+"] in ["+region+"] + ["+departement+"] between ["+minPrice+"] and ["+maxPrice+"] started ["+startDate+"]";
+	}
+	
+
+	
+	
+}
