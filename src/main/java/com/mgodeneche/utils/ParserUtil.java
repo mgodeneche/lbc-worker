2bb95ffff37d0bb7bfbc99dc180ea4098fb71f18
Godeneche Maxence <maxence.godeneche@gmail.com>
2017-07-09 19:00:56 +0200

    Tittle parsing OK

--- src/main/java/com/mgodeneche/utils/ParserUtil.java
+++ src/main/java/com/mgodeneche/utils/ParserUtil.java
@@ -9,12 +9,14 @@ import org.slf4j.LoggerFactory;
 import com.mgodeneche.entity.ResultItem;
 
 public class ParserUtil {
+	private final static String EMPTY ="";
 	private static final Logger logger = LoggerFactory.getLogger(ParserUtil.class);
 	
 	public  void parseHttpResponse(Document doc){
 		Elements items = doc.select(".item_infos");
 		for(Element item : items){
 			parseItemInfos(item);
+			//logger.info(item.toString());
 		}
 		logger.info(items.size()+" résultats trouvés");
 	}
@@ -22,17 +24,40 @@ public class ParserUtil {
 	private ResultItem parseItemInfos(Element itemInfos){
 		ResultItem rI = new ResultItem();
 		getItemTitle(itemInfos);
-		//rI.setTitle
+		//getPrice(itemInfos);
+		//getCategory(itemInfos);
+		
 		
 		return rI;
 	}
 	private String getItemTitle(Element itemInfos){
 		Elements items = itemInfos.select(".item_title");
 		for(Element item : items){
-			item.get
-			logger.info(item.toString());
+			String s = item.text();
+			if(s.length()<0){
+				return s;
+			}
 		}
 		
-		return null;
+		return EMPTY;
+	}
+	private String getCategory(Element itemInfos){
+		//TODO: compare with an enum ? 
+		Elements items = itemInfos.select(".item_supp");
+		for(Element item : items){
+			String s = item.text();
+			logger.info(s);
+		}
+		
+		return EMPTY;
+	}
+	private String getPrice(Element itemInfos){
+		Elements items = itemInfos.select(".item_price");
+		for(Element item : items){
+			String s = item.text();
+			logger.info(s);
+		}
+		
+		return EMPTY;
 	}
 }
